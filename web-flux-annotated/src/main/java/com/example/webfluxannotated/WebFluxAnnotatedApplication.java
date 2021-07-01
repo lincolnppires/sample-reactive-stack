a363c2b4ddbdf91eca42b40db0ad3753256d1967
Lincoln Pires <lincoln.pires@xpi.com.br>
2021-07-01 14:58:38 -0300

    initial commit - demo webflux annotated

---
+++ web-flux-annotated/src/main/java/com/example/webfluxannotated/WebFluxAnnotatedApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxannotated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxAnnotatedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxAnnotatedApplication.class, args);
+    }
+
+}
---
+++ web-flux-annotated/src/test/java/com/example/webfluxannotated/WebFluxAnnotatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxannotated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxAnnotatedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
